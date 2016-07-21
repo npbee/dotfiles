aea3f286907f3610847b785e49030efdaf393043
Nick Ball <nicholaspball@gmail.com>
2016-07-21 06:49:51 -0700

    change hyperterm theme

--- hyperterm.js
+++ hyperterm.js
@@ -55,7 +55,7 @@ module.exports = {
   //   `hypersolar`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-spacegray'],
+  plugins: ['hyperterm-gruvbox-dark'],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
