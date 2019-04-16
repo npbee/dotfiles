ffc2a5dffbcd827dfdb6d9761cc66ca6c1b8f6fc
Nick Ball <nick.ball@hellosign.com>
2019-04-16 07:42:19 -0700

    Remove tap dance on semicolon
    Seems to have issues with the z tapdance

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   q  |   w  |   e  |   r  |   t  |  [   |           |   ]  |   y  |   u  |   i  |   o  |   p  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   a  |   s  |   e  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  | ;/:  |   '    |
+ * | LCtrl  |   a  |   s  |   e  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |   ;  |   '    |
  * |--------+------+------+------+------+------|  {   |           |   }  |------+------+------+------+------+--------|
  * | LShift |   z  |   x  |   c  |   v  |   b  |      |           |      |   n  |   m  |   ,  |   .  |//L2 | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_PGUP,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         KC_RBRACKET, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRACKET,
-                     KC_H,   KC_J,   KC_K,   KC_L,   TD(CT_CLN),        KC_QUOTE,
+                     KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,        KC_QUOTE,
         KC_RCBR,     KC_N,   KC_M,   KC_COMM,KC_DOT, LT(MDIA, KC_SLSH),   KC_RSFT,
                              KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          MO(MDIA),
         KC_LALT, CTL_T(KC_ESC),
