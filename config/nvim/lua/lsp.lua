7d173d5668e83d77e68dbb17ce0b3c51a417904b
Nick Ball <nick.ball@dropbox.com>
2021-05-19 07:34:10 -0700

    Add flowlsp config

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -71,3 +71,15 @@ require'lspconfig'.tsserver.setup{
     )
   }
 }
+
+require'lspconfig'.flow.setup{
+  on_attach = on_attach,
+
+  handlers = {
+    ["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+      }
+    )
+  }
+}
