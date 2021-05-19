5051788fa33ec7845ff0a8b89259ecb2079087aa
Nick Ball <nick.ball@dropbox.com>
2021-05-19 07:37:59 -0700

    Add single normal mode VI command for ZSH

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -14,3 +14,14 @@ function vi_mode_prompt_info() {
 # define right prompt, regardless of whether the theme defined it
 RPS1='$(vi_mode_prompt_info)'
 RPS2=$RPS1
+
+# Bind control+O for a temporary normal mode command
+vi-cmd () {
+  local REPLY
+
+  zle .read-command -K vicmd &&
+    zle $REPLY -K vicmd
+}
+
+zle -N vi-cmd
+bindkey -v '^O' vi-cmd
