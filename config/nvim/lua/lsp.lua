c0077d6247210b83c7623d2f9a7cb60198fa6132
Nick Ball <nick.ball@dropbox.com>
2021-10-21 07:46:27 -0700

    Remove tsserver

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -59,15 +59,3 @@ vim.lsp.handlers["textDocument/signatureHelp"] =
     border = "single"
   }
 )
-
-require'lspconfig'.tsserver.setup{
-  on_attach = on_attach,
-
-  handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false
-      }
-    )
-  }
-}
