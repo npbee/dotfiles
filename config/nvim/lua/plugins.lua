a7214d56270c24a0f503676e34e976e0600e99d3
Nick Ball <nick.ball@dropbox.com>
2021-12-09 13:26:40 -0800

    Add cmp-cmdline

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -23,6 +23,7 @@ Plug 'folke/lsp-colors.nvim'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/nvim-cmp'
 
