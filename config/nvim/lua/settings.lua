a15d3874e23af40ad4956eedcc02f95315f2836d
Nick Ball <nick.ball@dropbox.com>
2021-12-10 08:09:25 -0800

    Try spell checking

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -21,6 +21,7 @@ local options = {
     secure = true,
     showmode = true,
     signcolumn = "yes", -- Always show sign column to avoid layout shift
+    spelllang = "en",
     splitbelow = true,
     synmaxcol = 200,
     syntax = "on",
