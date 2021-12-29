5612d9b3274e67611e4a7a2c2fbc39bbccb01b68
Nick Ball <nick@npbee.me>
2021-12-29 07:54:27 -0800

    Adjust local config loading

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,21 +1,22 @@
-require('settings')
-require('plugins')
-require('colors')
-require('lsp')
-require('keymappings')
-require('autocmds')
+require("settings")
+require("plugins")
+require("colors")
+require("lsp")
+require("keymappings")
+require("autocmds")
 
 vim.cmd([[
-function! ToggleFixOnSave()
-    if g:ale_fix_on_save
-        let g:ale_fix_on_save = 0
-    else
-        let g:ale_fix_on_save = 1
-    endif
-endfunction
+ function! ToggleFixOnSave()
+     if g:ale_fix_on_save
+         let g:ale_fix_on_save = 0
+     else
+         let g:ale_fix_on_save = 1
+     endif
+ endfunction
+]])
 
+function load_local()
+  require("init-local")
+end
 
-if filereadable(glob("~/.config/nvim/init.local.vim"))
-    source ~/.config/nvim/init.local.vim
-endif
-    ]])
+pcall(load_local)
