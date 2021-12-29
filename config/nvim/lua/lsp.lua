9136e20a9a24463a6a17ff00ab0dc53c961a5d74
Nick Ball <nick@npbee.me>
2021-12-29 05:24:22 -0800

    Add stylua

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,77 +1,64 @@
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
-local lspconfig = require('lspconfig')
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lspconfig = require("lspconfig")
 
 vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = false,
-    float = {
-        format = function(diagnostic)
-            return string.format("[%s] %s (%s)", diagnostic.source,
-                                 diagnostic.message, diagnostic.code)
-        end
-    }
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = false,
+	float = {
+		format = function(diagnostic)
+			return string.format("[%s] %s (%s)", diagnostic.source, diagnostic.message, diagnostic.code)
+		end,
+	},
 })
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = opts.border or "rounded"
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or "rounded"
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>wa',
-                   '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr',
-                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl',
-                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>D',
-                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                   opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-                   opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>q',
-                   '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
+	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
-    if client.resolved_capabilities.document_formatting then
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
+	if client.resolved_capabilities.document_formatting then
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+	end
 
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       hi LspReferenceRead cterm=bold ctermbg=red guibg=User5
       hi LspReferenceText cterm=bold ctermbg=red guibg=User5
       hi LspReferenceWrite cterm=bold ctermbg=red guibg=User5
@@ -80,56 +67,59 @@ local on_attach = function(client, bufnr)
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
-    end
+    ]],
+			false
+		)
+	end
 end
 
 -- Disabled until I'm actually using this somewhere
 lspconfig.tsserver.setup({
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-        -- Use prettier for formatting
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+	capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		-- Use prettier for formatting
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
 
-        on_attach(client, bufnr)
-    end,
+		on_attach(client, bufnr)
+	end,
 
-    handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic
-                                                               .on_publish_diagnostics,
-                                                           {
-            virtual_text = false
-        })
-    },
+	handlers = {
+		["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			virtual_text = false,
+		}),
+	},
 
-    filetypes = {
-        -- At work we use flow, so it's too slow to have both LSP servers running
-        -- Disabling JS files for now
-        -- 'javascript',
-        -- 'javascriptreact',
-        -- 'javascript.jsx',
-        'typescript', 'typescriptreact', 'typescript.tsx'
-    }
+	filetypes = {
+		-- At work we use flow, so it's too slow to have both LSP servers running
+		-- Disabling JS files for now
+		-- 'javascript',
+		-- 'javascriptreact',
+		-- 'javascript.jsx',
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
 })
 
-lspconfig.flow.setup({capabilities = capabilities, on_attach = on_attach})
+lspconfig.flow.setup({ capabilities = capabilities, on_attach = on_attach })
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.misspell,
-        null_ls.builtins.diagnostics.eslint_d,
-        -- null_ls.builtins.diagnostics.eslint_d.with({
-        --     command = 'eslintme',
-        --     args = {"$FILENAME"}
-        -- }), 
-        -- null_ls.builtins.formatting.prettier.with({
-        --     only_local = "node_modules/.bin"
-        -- })
-        null_ls.builtins.formatting.prettierd.with({
-            prefer_local = "node_modules/.bin"
-        })
-    },
+	sources = {
+		null_ls.builtins.diagnostics.misspell,
+		null_ls.builtins.diagnostics.eslint_d,
+		-- null_ls.builtins.diagnostics.eslint_d.with({
+		--     command = 'eslintme',
+		--     args = {"$FILENAME"}
+		-- }),
+		-- null_ls.builtins.formatting.prettier.with({
+		--     only_local = "node_modules/.bin"
+		-- })
+		null_ls.builtins.formatting.prettierd.with({
+			prefer_local = "node_modules/.bin",
+		}),
+		null_ls.builtins.formatting.stylua,
+	},
 
-    on_attach = on_attach
+	on_attach = on_attach,
 })
