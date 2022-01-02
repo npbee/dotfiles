3ec9e8cc6d672540af6bb48e1ecfa5f6490c23d8
Nick Ball <nick@npbee.me>
2022-01-02 09:08:23 -0800

    TS updates

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -83,7 +83,7 @@ require("plugins.gitsigns")
 require("gitlinker").setup()
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "javascript" },
+  ensure_installed = { "javascript", "typescript" },
 
   highlight = {
     -- `false` will disable the whole extension
@@ -96,7 +96,7 @@ require("nvim-treesitter.configs").setup({
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { "javascriptreact" },
+    additional_vim_regex_highlighting = { "javascriptreact", "javascript", "typescript", "typescriptreact" },
   },
 })
 
