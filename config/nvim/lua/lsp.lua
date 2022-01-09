a07dd78be17c2a83064aded156accf9f1c286435
Nick Ball <nick@npbee.me>
2022-01-09 10:21:41 -0800

    Update write_good file types

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -119,7 +119,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.diagnostics.write_good.with({
-      filetypes = {},
+      filetypes = { "gitcommit", "markdown" },
     }),
     null_ls.builtins.formatting.prettierd.with({
       prefer_local = "node_modules/.bin",
