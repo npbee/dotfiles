65315bcfeb0804a3d1d8bd5a126a4705f3904d9a
Nick Ball <nick@npbee.me>
2022-07-11 08:00:45 -0700

    updates

--- config/nvim/lua/plugins/vsnip.lua
+++ config/nvim/lua/plugins/vsnip.lua
@@ -1,5 +1,5 @@
 vim.g.vsnip_filetypes = {
   javascriptreact = { "javascript" },
-  typescriptreact = { "typescript", "javascript" },
+  typescriptreact = { "typescript", "javascript", "javascriptreact" },
   typescript = { "typescript", "javascript" },
 }
