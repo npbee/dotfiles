716deccc330b5808055ae694b4dd7a7f12b5fe85
Nick Ball <nick@npbee.me>
2022-10-23 13:13:01 -0700

    Autopairs

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -243,6 +243,12 @@ require("packer").startup(function(use)
       }
     end
   }
+  use({
+    "windwp/nvim-autopairs",
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  })
 
   if packer_bootstrap then
     require("packer").sync()
