688fddece1148ca3b6f4c0d942c6e2f50541094e
Nick Ball <nick@npbee.me>
2023-09-26 08:18:39 -0700

    remove emmet

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -228,38 +228,6 @@ return {
         cmd = { vim.fn.expand("$HOME/.bin/elixir-ls/language_server.sh") }
       })
 
-      if not configs.ls_emmet then
-        configs.ls_emmet = {
-          default_config = {
-            cmd = { "ls_emmet", "--stdio" },
-            filetypes = {
-              "html",
-              "css",
-              "scss",
-              "javascriptreact",
-              "typescriptreact",
-              "haml",
-              "xml",
-              "xsl",
-              "pug",
-              "slim",
-              "sass",
-              "stylus",
-              "less",
-              "sss",
-              "hbs",
-              "handlebars",
-            },
-            root_dir = function()
-              return vim.loop.cwd()
-            end,
-            settings = {},
-          },
-        }
-      end
-
-      lspconfig.ls_emmet.setup({ capabilities = capabilities })
-
       lspconfig.svelte.setup({ on_attach = on_attach })
     end,
   },
