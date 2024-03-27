cca77c9aa03607816cd3560e7236171869485fff
Nick Ball <nick@npbee.me>
2024-03-27 14:12:21 -0700

    play with cmp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -5,40 +5,30 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
-    "saadparwaiz1/cmp_luasnip"
+    "saadparwaiz1/cmp_luasnip",
+    "onsails/lspkind.nvim",
+    "ray-x/cmp-treesitter",
   },
   config = function()
     local cmp = require("cmp")
+    local lspkind = require("lspkind")
 
-    local icons = {
-      Class = " ",
-      Color = " ",
-      Constant = "π ",
-      Constructor = " ",
-      Enum = "了",
-      EnumMember = " ",
-      Field = " ",
-      File = " ",
-      Folder = " ",
-      Function = " ",
-      Interface = "ﰮ ",
-      Keyword = " ",
-      Method = "ƒ ",
-      Module = " ",
-      Property = " ",
-      Snippet = "✂ ",
-      Struct = " ",
-      Text = " ",
-      Unit = " ",
-      Value = " ",
-      Variable = " ",
+    local source_mapping = {
+      nvim_lsp = "[LSP]",
+      nvim_lua = "[LUA]",
+      luasnip = "[SNIP]",
+      buffer = "[BUF]",
+      path = "[PATH]",
+      treesitter = "[TREE]",
+      cmp_ai = "[AI]",
+      ["vim-dadbod-completion"] = "[DB]",
     }
 
     cmp.setup({
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          require('luasnip').lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -56,39 +46,34 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
-        { name = "nvim_lsp", },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "buffer",   keyword_length = 5 }
+        { name = "luasnip", group_index = 1 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "path", group_index = 3 },
+        { name = "treesitter", keyword_length = 4, group_index = 4 },
+        { name = "buffer", keyword_length = 5, group_index = 4 },
       }),
 
       window = {
-        documentation = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-        },
-        completion = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-        },
+        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
       },
 
       performance = {
-        async_budget = 10
+        async_budget = 10,
       },
 
       formatting = {
-        format = function(_, vim_item)
-          if icons[vim_item.kind] then
-            vim_item.kind = icons[vim_item.kind] .. " " .. vim_item.kind
-          end
-          return vim_item
-        end,
+        format = lspkind.cmp_format({
+          mode = "text",
+          ellipsis_char = "...",
+          menu = source_mapping,
+        }),
       },
 
       experimental = {
-        native_menu = false
-      }
+        native_menu = false,
+        ghost_text = false,
+      },
     })
 
     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
@@ -101,5 +86,5 @@ return {
         -- Your configuration here.
       }),
     })
-  end
+  end,
 }
