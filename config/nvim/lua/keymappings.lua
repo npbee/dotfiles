a0adf428bd08439b7866cc6f215b032a61624adf
Nick Ball <nick@npbee.me>
2024-03-29 12:10:33 -0700

    Try harpoon

--- config/nvim/lua/keymappings.lua
+++ config/nvim/lua/keymappings.lua
@@ -1,6 +1,6 @@
 -- Easier split navigation
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true })
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true })
+-- vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true })
+-- vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true })
 vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { noremap = true })
 vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { noremap = true })
 
