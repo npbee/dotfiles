be907b61d6d5d46233ab68cd39a925d45f7d2a65
Nick Ball <nick@npbee.me>
2024-05-10 09:07:51 -0700

    Update elixir-ls

--- bin/elixir-ls/launch.sh
+++ bin/elixir-ls/launch.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Actual launcher. This does the hard work of figuring out the best way
-# to launch the language server or the debugger.
-#
+# to launch the language server or the debug adapter.
 
 # Running this script is a one-time action per project launch, so we opt for
 # code simplicity instead of performance. Hence some potentially redundant
@@ -11,39 +10,74 @@
 
 did_relaunch=$1
 
+# Get the user's preferred shell
+preferred_shell=$(basename "$SHELL")
+
+# Get current dirname
+dirname=$(dirname "$0")
+
+case "${did_relaunch}" in
+  "")
+    if [ "$preferred_shell" = "bash" ]; then
+      >&2 echo "Preferred shell is bash, relaunching"
+      exec "$(which bash)" "$0" relaunch
+    elif [ "$preferred_shell" = "zsh" ]; then
+      >&2 echo "Preferred shell is zsh, relaunching"
+      exec "$(which zsh)" "$0" relaunch
+    elif [ "$preferred_shell" = "fish" ]; then
+      >&2 echo "Preferred shell is fish, launching launch.fish"
+      exec "$(which fish)" "$dirname/launch.fish"
+    else
+      >&2 echo "Preferred shell $preferred_shell is not supported, continuing in POSIX shell"
+    fi
+    ;;
+  *)
+    # We have an arg2, so we got relaunched
+    ;;
+esac
+
 ASDF_DIR=${ASDF_DIR:-"${HOME}/.asdf"}
 
 asdf_vm="${ASDF_DIR}/asdf.sh"
+
+>&2 echo "Looking for ASDF install"
 if test -f "${asdf_vm}"
 then
-  # asdf-vm does not support the plain posix shell. Figure out
-  # which one we need and relaunch ourselves with that.
-  case "${did_relaunch}" in
-    "")
-      if which bash >/dev/null
-      then
-        exec "$(which bash)" "$0" relaunch
-      elif which zsh >/dev/null
-      then
-        exec "$(which zsh)" "$0" relaunch
-      fi
-      ;;
-    *)
-      # We have an arg2, so we got relaunched. Therefore, we're running in a
-      # shell that supports asdf-vm.
-      .  "${asdf_vm}"
-      ;;
-  esac
+  >&2 echo "ASDF install found in $asdf_vm, sourcing"
+  # shellcheck disable=SC1090
+  .  "${asdf_vm}"
+else
+  >&2 echo "ASDF not found"
+  >&2 echo "Looking for mise executable"
+
+  if which mise >/dev/null
+  then
+    >&2 echo "mise executable found in $(which mise), activating"
+    eval "$($(which mise) env -s "$preferred_shell")"
+  else
+    >&2 echo "mise not found"
+    >&2 echo "Looking for rtx executable"
+
+    if which rtx >/dev/null
+    then
+      >&2 echo "rtx executable found in $(which rtx), activating"
+      eval "$($(which rtx) env -s "$preferred_shell")"
+    else
+      >&2 echo "rtx not found"
+    fi
+  fi
 fi
 
 # In case that people want to tweak the path, which Elixir to use, or
-# whatever prior to launching the language server or the debugger, we
+# whatever prior to launching the language server or the debug adapter, we
 # give them the chance here. ELS_MODE will be set for
 # the really complex stuff. Use an XDG compliant path.
 
-els_setup="${XDG_CONFIG_HOME:-~/.config}/elixir_ls/setup.sh"
+els_setup="${XDG_CONFIG_HOME:-$HOME/.config}/elixir_ls/setup.sh"
 if test -f "${els_setup}"
 then
+  >&2 echo "Running setup script $els_setup"
+  # shellcheck disable=SC1090
   .  "${els_setup}"
 fi
 
@@ -68,6 +102,22 @@ else
   SCRIPTPATH=${ELS_INSTALL_PREFIX}
 fi
 
-export ERL_LIBS="$SCRIPTPATH:$ERL_LIBS"
+export MIX_ENV=prod
+# Mix.install prints to stdout and reads from stdin
+# we need to make sure it doesn't interfere with LSP/DAP
+echo "" | elixir "$SCRIPTPATH/quiet_install.exs" >/dev/null || exit 1
 
-exec elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none"  -e "$ELS_SCRIPT"
+default_erl_opts="-kernel standard_io_encoding latin1 +sbwt none +sbwtdcpu none +sbwtdio none"
+
+if [ "$preferred_shell" = "bash" ]; then
+  source "$dirname/exec.bash"
+elif [ "$preferred_shell" = "zsh" ]; then
+  source "$dirname/exec.zsh"
+else
+  if [ -z "$ELS_ELIXIR_OPTS" ]
+  then
+    # in posix shell does not support arrays
+    >&2 echo "ELS_ELIXIR_OPTS is not supported in current shell"
+  fi
+  exec elixir --erl "$default_erl_opts $ELS_ERL_OPTS" "$SCRIPTPATH/launch.exs"
+fi
