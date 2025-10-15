1e70e1f1263b6d21481df0b23c7d692b6b4070e3
Nick Ball <nick@npbee.me>
2025-10-15 06:44:19 -0700

    Mini.pick config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -250,12 +250,26 @@ lazy.setup({
       'nvim-mini/mini.pick',
       version = '*',
       config = function()
+        local win_config = function()
+          local height = math.floor(0.618 * vim.o.lines)
+          local width = math.floor(0.618 * vim.o.columns)
+          return {
+            anchor = 'NW',
+            height = height,
+            width = width,
+            row = math.floor(0.5 * (vim.o.lines - height)),
+            col = math.floor(0.5 * (vim.o.columns - width)),
+          }
+        end
         require('mini.pick').setup({
           mappings = {
             move_up = '<C-k>',
             move_down = '<C-j>',
             toggle_preview = '<C-p>',
           },
+          window = {
+            config = win_config
+          }
 
         })
       end
