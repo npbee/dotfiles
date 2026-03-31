849ddbd0512f73c2e7b2ac7968cc41a74a58d315
Nick Ball <nick@npbee.me>
2026-03-31 06:55:15 -0700

    Fix nvim-treesitter for Neovim 0.12.0
    Switch to main branch and use new setup API (nvim-treesitter.configs
    was removed in the main branch).

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   cmd = { "TSUpdateSync" },
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter").setup({
       ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "scss", "json", "graphql" },
     })
   end,
